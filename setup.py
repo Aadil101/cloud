232a62151f2cc2ce481ff48681dc90ea56af29ab
Aadil <aadilislam101@gmail.com>
2019-08-02 20:26:43 -0400

    Implemented page pagination

--- setup.py
+++ setup.py
@@ -12,8 +12,10 @@ setup(
         'boxsdk',
         'onedrivesdk',
         'onedrivecmd',
-        'keyring',
         'requests',
         'datetime',
+        'keyring',
+        'absl-py',
+        'windows-curses',
     ],
 )
